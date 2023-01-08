261e166632cc3217dbd51643dae380950742ac0a
Muthuselvaganesh <122195360+Muthuselvaganesh@users.noreply.github.com>
2023-01-08 11:24:25 +0530

    Add files via upload

---
+++ day1/PrintOddNumbers.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+import java.util.Iterator;
+
+public class PrintOddNumbers {
+	
+	public_static_void main(String[] args) {
+		
+		int i ;
+		
+		for (int i=1; i<=21; i++) {
+		
+		if(i%2!=0) {
+			System.out.println(i);
+		}
+		
+		}
+		
+				
+				
+
+	}
+
+}
